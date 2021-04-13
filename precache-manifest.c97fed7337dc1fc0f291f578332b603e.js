ccc316c38bf51fae7887c1f7bd93b614bbcfc2b3
Alexa King <kingjalexa@gmail.com>
2021-04-13 19:36:18 -0400

    Updates

---
+++ precache-manifest.c97fed7337dc1fc0f291f578332b603e.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bb6697b1d1d046b3c869b9416975585",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "c98d296647b7295a9440",
+    "url": "/portfolio/static/css/3.3a26ebde.chunk.css"
+  },
+  {
+    "revision": "a2b298f9768df1a3136b",
+    "url": "/portfolio/static/css/4.ea3dbbf2.chunk.css"
+  },
+  {
+    "revision": "349b7b12fbc648809394",
+    "url": "/portfolio/static/css/main.fb5b0910.chunk.css"
+  },
+  {
+    "revision": "57dd8305b7505f9e970d",
+    "url": "/portfolio/static/js/2.7c24801a.chunk.js"
+  },
+  {
+    "revision": "0a6d64cd3b49be6a14afdf21fa7c6d43",
+    "url": "/portfolio/static/js/2.7c24801a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c98d296647b7295a9440",
+    "url": "/portfolio/static/js/3.c97650b5.chunk.js"
+  },
+  {
+    "revision": "a2b298f9768df1a3136b",
+    "url": "/portfolio/static/js/4.c0f6dadb.chunk.js"
+  },
+  {
+    "revision": "349b7b12fbc648809394",
+    "url": "/portfolio/static/js/main.53e12a50.chunk.js"
+  },
+  {
+    "revision": "eaf42a2b2ef02abe52a0",
+    "url": "/portfolio/static/js/runtime-main.83074172.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/portfolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/portfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "0b7b9dcf3cf4f27a9d161b0704c5d65f",
+    "url": "/portfolio/static/media/QRCode 2-02.0b7b9dcf.png"
+  },
+  {
+    "revision": "48c6335be5bacb7779b644b7dfbbb453",
+    "url": "/portfolio/static/media/alexacafe.48c6335b.jpg"
+  },
+  {
+    "revision": "f3e6129cdf0674d188102979e784aff1",
+    "url": "/portfolio/static/media/alexakingbranding-02.f3e6129c.png"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/portfolio/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/portfolio/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/portfolio/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "93d0ca332145a2883f371d9ee4f63fff",
+    "url": "/portfolio/static/media/gracehopper.93d0ca33.png"
+  },
+  {
+    "revision": "829ad0a1deef4d7295adfecdf12df2be",
+    "url": "/portfolio/static/media/nextuLogo.829ad0a1.webp"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/portfolio/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "25018d29d5ab115bc51c3e9a2d92f90f",
+    "url": "/portfolio/static/media/saayaHealthLogo.25018d29.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/portfolio/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "39eff4a91b01e00a32cb78f3797eec88",
+    "url": "/portfolio/static/media/talksCardBack.39eff4a9.svg"
+  }
+]);
